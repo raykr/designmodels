6759cf3397e1269e331cbed16f8badaa88ea4367
jingz <jingzonglei@163.com>
2017-01-12 16:11:17 +0800

    23 degin models

--- src/com/swroom/creator/singleton/EagerSingleton.java
+++ src/com/swroom/creator/singleton/EagerSingleton.java
@@ -1,34 +1,30 @@
-package com.swroom.singleton;
+package com.swroom.creator.singleton;
 
 /**
- * 懒汉式单例模式
+ * 饿汉式单例模式
  *
  * Created by jingz on 2017/1/12.
  */
-public class LazySingleton {
+public class EagerSingleton {
 
     /**
      * 指向自己实例的私有静态引用
      * 目的：全局可用，不被其他类实例化
      */
-    private static LazySingleton singleton;
+    private static EagerSingleton singleton = new EagerSingleton();
 
     /**
      * 私有构造方法
      * 目的：不可继承，保证唯一性
      */
-    private LazySingleton() {}
+    private EagerSingleton() {}
 
     /**
      * 以自己实例为返回值的静态的公有的方法
      * 目的：提供给外部使用
-     * synchronized关键字保证线程安全
      * @return singleton
      */
-    public static synchronized LazySingleton getInstance() {
-        if (singleton == null) {
-            singleton = new LazySingleton();
-        }
+    public static EagerSingleton getInstance() {
         return singleton;
     }
 }
--- src/com/swroom/creator/singleton/LazySingleton.java
+++ src/com/swroom/creator/singleton/LazySingleton.java
@@ -1,30 +1,34 @@
-package com.swroom.singleton;
+package com.swroom.creator.singleton;
 
 /**
- * 饿汉式单例模式
+ * 懒汉式单例模式
  *
  * Created by jingz on 2017/1/12.
  */
-public class EagerSingleton {
+public class LazySingleton {
 
     /**
      * 指向自己实例的私有静态引用
      * 目的：全局可用，不被其他类实例化
      */
-    private static EagerSingleton singleton = new EagerSingleton();
+    private static LazySingleton singleton;
 
     /**
      * 私有构造方法
      * 目的：不可继承，保证唯一性
      */
-    private EagerSingleton() {}
+    private LazySingleton() {}
 
     /**
      * 以自己实例为返回值的静态的公有的方法
      * 目的：提供给外部使用
+     * synchronized关键字保证线程安全
      * @return singleton
      */
-    public static EagerSingleton getInstance() {
+    public static synchronized LazySingleton getInstance() {
+        if (singleton == null) {
+            singleton = new LazySingleton();
+        }
         return singleton;
     }
 }
